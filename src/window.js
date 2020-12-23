da03a88f86483cc35ad04c09672cfd3da8f0f8cb
Etienne Dupont <etienne.dupont@sterblue.com>
2020-12-23 23:54:29 +0100

    Refactor window

--- src/window.js
+++ src/window.js
@@ -1,3 +1,7 @@
+/**
+ * This script contains the relevant polyfills for the DOM window object.
+ * It is used by the node build of three so that three.js can run on NodeJs
+ */
 import { JSDOM } from "jsdom";
 
 const jsdomOptions = {
@@ -13,43 +17,69 @@ const jsdom = new JSDOM(
 );
 
 const window = jsdom.window;
-export const Blob = window.Blob;
-export const atob = window.atob;
-export const DOMParser = window.DOMParser;
-export const document = window.document;
-export const XMLHttpRequest = window.XMLHttpRequest;
-export const TextEncoder = window.TextEncoder;
-export const TextDecoder = window.TextDecoder;
-export const decodeURIComponent = window.decodeURIComponent;
-export const CustomEvent = window.CustomEvent;
-export const WebGLRenderingContext = window.WebGLRenderingContext;
-export const WebGL2RenderingContext = window.WebGL2RenderingContext;
-export const innerWidth = window.innerWidth;
-export const innerHeight = window.innerHeight;
-export const DeviceOrientationEvent = window.DeviceOrientationEvent;
-export const orientation = window.orientation;
-export const addEventListener = window.addEventListener;
-export const removeEventListener = window.removeEventListener;
-export const focus = window.focus;
-export const pageXOffset = window.pageXOffset;
-export const pageYOffset = window.pageYOffset;
-export const FileReader = window.FileReader;
-export const URL = window.URL;
-export const ActiveXObject = window.ActiveXObject;
-export const AudioContext = window.AudioContext;
-export const webkitAudioContext = window.webkitAudioContext;
-export const XRHand = window.XRHand;
-export const HTMLImageElement = window.HTMLImageElement;
-export const HTMLCanvasElement = window.HTMLCanvasElement;
-export const ImageBitmap = window.ImageBitmap;
-export const createImageBitmap = window.createImageBitmap;
-export const self = window.self;
+const Blob = window.Blob;
+const atob = window.atob;
+const DOMParser = window.DOMParser;
+const document = window.document;
+const XMLHttpRequest = window.XMLHttpRequest;
+const TextEncoder = window.TextEncoder;
+const TextDecoder = window.TextDecoder;
+const decodeURIComponent = window.decodeURIComponent;
+const CustomEvent = window.CustomEvent;
+const WebGLRenderingContext = window.WebGLRenderingContext;
+const WebGL2RenderingContext = window.WebGL2RenderingContext;
+const innerWidth = window.innerWidth;
+const innerHeight = window.innerHeight;
+const DeviceOrientationEvent = window.DeviceOrientationEvent;
+const orientation = window.orientation;
+const addEventListener = window.addEventListener;
+const removeEventListener = window.removeEventListener;
+const focus = window.focus;
+const pageXOffset = window.pageXOffset;
+const pageYOffset = window.pageYOffset;
+const FileReader = window.FileReader;
+const URL = window.URL;
+const ActiveXObject = window.ActiveXObject;
+const AudioContext = window.AudioContext;
+const webkitAudioContext = window.webkitAudioContext;
+const XRHand = window.XRHand;
+const HTMLImageElement = window.HTMLImageElement;
+const HTMLCanvasElement = window.HTMLCanvasElement;
+const ImageBitmap = window.ImageBitmap;
+const createImageBitmap = window.createImageBitmap;
+const self = window.self;
 
-export const namesGlobal = [
-	"Blob", "atob", "DOMParser", "document", "XMLHttpRequest", "TextEncoder", "TextDecoder",
-	"decodeURIComponent", "CustomEvent", "WebGLRenderingContext", "WebGL2RenderingContext",
-	"innerWidth", "innerHeight", "DeviceOrientationEvent", "orientation", "addEventListener",
-	"removeEventListener", "focus", "pageXOffset", "pageYOffset", "FileReader", "URL",
-	"ActiveXObject", "AudioContext", "webkitAudioContext", "XRHand", "HTMLImageElement",
-	"HTMLCanvasElement", "ImageBitmap", "createImageBitmap", "self"
-];
+export {
+	window,
+	Blob,
+	atob,
+	DOMParser,
+	document,
+	XMLHttpRequest,
+	TextEncoder,
+	TextDecoder,
+	decodeURIComponent,
+	CustomEvent,
+	WebGLRenderingContext,
+	WebGL2RenderingContext,
+	innerWidth,
+	innerHeight,
+	DeviceOrientationEvent,
+	orientation,
+	addEventListener,
+	removeEventListener,
+	focus,
+	pageXOffset,
+	pageYOffset,
+	FileReader,
+	URL,
+	ActiveXObject,
+	AudioContext,
+	webkitAudioContext,
+	XRHand,
+	HTMLImageElement,
+	HTMLCanvasElement,
+	ImageBitmap,
+	createImageBitmap,
+	self
+};
